ff5a6985259ea81e315991349c951221b32afce7
Mattias <mattias1126@protonmail.com>
2024-12-28 15:06:49 +0000

    Remove generic `drawing.el` file and replace with `hex-drawing.el`
    Also adds a wip scratch where we're trying to get the roads drawing working nicely

---
+++ hex-drawing.el
@@ -0,0 +1,49 @@
+;;; hex-drawing.el --- Draw hexes as SVGs, for use with hexmap-mode.el
+;; Author: Mattias <mattias1126@protonmail.com>
+;; Maintainer: Mattias <mattias1126@protonmail.com>
+;; Keywords: hexes mapping ttrpg svg image
+;; Version 0.1.0
+;; URL: <todo: include url>
+;; Package-Requires: ((emacs "29.0.50") (svg "1.1") (hexmap-mode "0.1.0"))
+
+;;; Commentary:
+
+;; Provides a set of functions to draw the hexes as represented in `.hexmap' files
+
+;;; Code:
+(require 'svg)
+(require 'hexes)
+
+(defun hex-draw (svg x y size &optional fill label)
+  "Draw a hex in SVG with center of X, Y and specified SIZE.
+Optionally provide the FILL and LABEL for the hex."
+  (let ((fill (or fill "pink"))
+	(label (or label "")))
+    (svg-polygon svg (hexes-flat-corners x y size)
+		 :stroke-color "none" :fill-color fill)
+    (svg-text svg label :x x :y y :font-size (/ size 2) :text-anchor "middle")))
+
+
+(defun hex-draw-axial (svg q r size &optional canvas fill label)
+  "Draw a hex on an SVG, accepts axial Q and R coordinates.
+Needs to provide the SIZE for converting to cartesian coordinates.
+CANVAS can be provided to calculate the center of 0,0.
+FILL and LABEL can also be provided, see `draw-hex' for details.
+Example usage: \(let ((svg (svg-create 200 200)))
+  (hex-draw-axial svg 0 0 20 200 \"green\")
+  (hex-draw-axial svg 0 1 20 200 \"green\")
+  (with-current-buffer (get-buffer-create \"*SVG Image: Testing*\")
+    (erase-buffer)
+    (insert-image (svg-image svg))
+    (display-buffer \"*SVG Image: Testing*\")))"
+  (let ((coords (hexes-axial-to-cartesian q r size))
+	(center (or canvas 0))
+	(fill (or fill "pink")))
+    (hex-draw svg
+	      (+ (/ center 2) (car coords))
+	      (+ (/ center 2) (cdr coords))
+	      size
+	      fill label)))
+
+(provide 'hex-drawing)
+;;; hex-drawing.el ends here
