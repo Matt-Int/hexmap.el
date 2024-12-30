092a6bc67141a434276006e51cc3bcc82af354f4
Mattias <mattias1126@protonmail.com>
2024-12-30 02:06:06 +0000

    Add roads drawing between hex entry points and other features

--- wip-drawing-roads.el
+++ wip-drawing-roads.el
@@ -4,10 +4,12 @@
 
 (let ((svg (svg-create 400 400)))
   (hex-draw-axial svg 0 0 80 400 "green")
+  (hex-draw-road svg 200 200 80
+		 '(:q 0 :r 0 :index 2 :feature nil)
+		 '(:q 0 :r 0 :index 1 :feature nil) "#ff3333" 2)
   (hex-draw-road svg 200 200 80 4 0 "blue" 4)
-  (hex-draw-road svg 200 200 80 0 3 "#333333" 4)
   (hex-draw-feature-axial svg 0 0 80 0 'village 400)
-  (dotimes (i 10)
+  (dotimes (i 2)
     (hex-draw-feature-axial svg 0 0 80 (1+ i) nil 400))
 (with-current-buffer (get-buffer-create "*SVG Image: Testing*")
     (erase-buffer)
