de95dd7678bfb405d65588d9c04e68959d7c469a
Mattias <mattias1126@protonmail.com>
2025-05-17 14:16:58 +0100

    Remove stroke-color from hex label coordinates

--- hex-drawing.el
+++ hex-drawing.el
@@ -32,7 +32,8 @@ Optionally provide the FILL, STROKE, and LABEL for the hex."
     (svg-polygon svg (hexes-flat-corners x y size)
 		 :stroke-color stroke :fill-color fill
 		 :stroke-width (* size (/ 1.0 80.0)))
-    (svg-text svg label :x x :y (+ y (* size 0.8)) :font-size (/ size 10) :text-anchor "middle" :stroke "black")))
+    (svg-text svg label :x x :y (+ y (* size 0.8)) :font-size (/ size 10)
+	      :text-anchor "middle")))
 
 
 (defun hex-draw-axial (svg q r size &optional canvas fill stroke label offset)
